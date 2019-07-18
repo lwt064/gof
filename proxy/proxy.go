23d96710651246e45d27ee514e116ab650115581
liuwentao01 <liuwentao01@baidu.com>
2019-07-18 18:08:05 +0800

    ok

---
+++ proxy/proxy.go
@@ -0,0 +1,26 @@
+package proxy
+
+import "fmt"
+
+// 用户找工作
+
+type Company interface {
+	Employ(uid int)
+}
+
+type CompanyX struct {
+}
+
+func (x *CompanyX) Employ(uid int) {
+	fmt.Println("cons! you was employed.")
+}
+
+type Proxy struct {
+	c CompanyX
+}
+
+// 可以控制对真实对象的访问（比如条件检测）或实现一些附加操作，对扩展开放，对修改封闭
+func (p *Proxy) Employ(uid int) {
+	p.c.Employ(uid)
+	fmt.Println("uid[%d] was employed.", uid)
+}
